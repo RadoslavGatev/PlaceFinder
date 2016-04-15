b2bdab0596952570cc39476ab8000b9319ed8516
RadoslavGatev <radoslav_gatev@abv.bg>
2016-04-15 23:29:54 +0300

    Moved the index name to web config

--- PlaceFinder/Services/PlaceService.cs
+++ PlaceFinder/Services/PlaceService.cs
@@ -14,33 +14,28 @@ namespace PlaceFinder.Services
     {
         private readonly SearchIndexClient _indexClient;
         private readonly SearchServiceClient _searchClient;
-        private readonly string IndexName = "places";
 
         public PlaceService()
         {
-            string searchServiceName =
-                ConfigurationManager.AppSettings["SearchServiceName"];
-            string apiKey =
-                ConfigurationManager.AppSettings["SearchServiceApiKey"];
+            string searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
+            string apiKey = ConfigurationManager.AppSettings["SearchServiceApiKey"];
+            string indexName = ConfigurationManager.AppSettings["SearchIndex"];
 
             // Create an HTTP reference to the catalog index
             _searchClient = new SearchServiceClient(searchServiceName,
                 new SearchCredentials(apiKey));
-            _indexClient = _searchClient.Indexes.GetClient(IndexName);
+            _indexClient = _searchClient.Indexes.GetClient(indexName);
         }
 
-        public async Task<DocumentSearchResult<Place>> Search(string query, bool typeFacet, string typeFilter)
+        public async Task<DocumentSearchResult<Place>> Search(string query, bool typeFacet,
+            string typeFilter)
         {
             SearchParameters sp = new SearchParameters()
             {
                 SearchMode = SearchMode.Any,
                 Top = 100,
                 // Add count
-                IncludeTotalResultCount = true,
-                // Add search highlights
-                //HighlightFields = new List<String>() { "nameBg" },
-                //HighlightPreTag = "<b>",
-                //HighlightPostTag = "</b>"
+                IncludeTotalResultCount = true
             };
 
             if (typeFilter != null)
